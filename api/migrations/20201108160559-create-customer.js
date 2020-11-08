10c3936391ac3b10cdf17129e9737cc4e8f56f4b
yomapi <yomapi.new@gmail.com>
2020-11-09 01:16:12 +0900

    [Customer] migration, model

---
+++ api/migrations/20201108160559-create-customer.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return Promise.all([
+      await queryInterface.createTable('Customers', {
+        id: {
+          type: Sequelize.STRING(191),
+          unique:true
+        },
+        password: {
+          type: Sequelize.STRING(191)
+        },
+        name: {
+          type: Sequelize.STRING(191)
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        deletedAt: {
+          type: Sequelize.DATE
+        }
+      }, {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      }),
+    ])
+
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Customers');
+  }
+};
---
+++ api/models/customer.js
@@ -0,0 +1,20 @@
+'use strict'
+const { sequelize } = require('.')
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+    class Customer extends Model { }
+    Customer.init({
+        id: {
+            type: DataTypes.STRING(191),
+            primaryKey: true,
+        },
+        password: {
+            type: DataTypes.STRING(191),
+        }
+    }, {
+        sequelize,
+        paranoid: true,
+        modelName: 'Customer'
+    })
+    return Customer
+}
