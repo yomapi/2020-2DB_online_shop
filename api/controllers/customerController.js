ccf7ed5ce82140c005af1fbdf28ab4fbceb87855
yomapi <yomapi.new@gmail.com>
2020-11-17 02:50:12 +0900

    [customer] signUp

--- api/controllers/customerController.js
+++ api/controllers/customerController.js
@@ -16,62 +16,38 @@ exports.CustomerInfo = async (req, res) => {
         result.sucess = true
         return res.status(200).json(result)
     } else {
-        result.code = 404
-        result.data = null
-        result.sucess = false
-        result.message = "Customer Not Found"
-        return res.status(404).json(result)
+        message = "Customer Not Found"
+        return res.status(404).json( {message} )
     }
 }
 
 exports.CustomerUpdate = async (req, res) => {
-    const id = req.body.id
+    const id = req.params.userId
     let customer = null
-    let result = {
-        code: 500,
-        data: null,
-        message: "",
-        sucess: false
-    }
-
+    //TODO: body validation
     customer = await Customer.findByPk(id)
     if (!customer) {
-        result.code = 404
-        result.data = null
-        result.sucess = false
-        result.message = "Customer Not Found"
-        return res.status(404).json(result)
+        message = "Customer Not Found"
+        return res.status(404).json( {message} )
     } else {
         await customer.update(req.body)
-        result.code = 201
-        result.data = customer
-        result.sucess = true
-        return res.status(201).json(result)
+        return res.status(201).json(customer)
     }
 }
 
 exports.CustomerCreate = async (req, res) => {
     const id = req.body.id
+    const pwd = req.body.pwd
+    const name = req.body.name
+    //TODO: 넘겨온 값 validation
+    
     let customer = null
-    let result = {
-        code: 500,
-        data: null,
-        message: "",
-        sucess: false
-    }
     customer = await Customer.findByPk(id)
     if (customer) {
-        result.code = 400
-        result.data = null
-        result.sucess = false
-        result.message = "User ID already Used"
-        return res.status(400).json(result)
+        return res.status(400).json({message: "User ID already Used"})
     } else {
         customer = await Customer.create(req.body)
-        result.code = 201
-        result.data = customer
-        result.sucess = true
-        return res.status(201).json(result)
+        return res.status(201).json(customer)
     }
 }
 
--- api/routes/customerRouter.js
+++ api/routes/customerRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController')
 
-router.post('/customer/info', customerController.CustomerInfo)
-router.post('/customer/update', customerController.CustomerUpdate)
-router.post('/customer/create', customerController.CustomerCreate)
-router.post('/customer/delete', customerController.CustomerDelete)
+//router.post('/info', customerController.CustomerInfo)
+//router.put('/customer/:userId', customerController.CustomerUpdate)
+router.post('/signUp', customerController.CustomerCreate)
+//router.post('/customer/delete', customerController.CustomerDelete)
 module.exports = router;
